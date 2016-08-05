1b9cd396d95d11d090f839c294ccaa10f4bca3d5
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-05 16:57:17 -0400

    bootstrap app component

---
+++ src/client/app.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import AppComponent from './app.component.js';
+// import Components from './components/components';
+import './global.scss';
+
+angular.module('myApp', [])
+
+// angular.module('myApp', [
+//   Components.name
+// ])
+.directive('app', AppComponent);
