fc0dc9ee1bf4dff68096fcaaed6259746f0d94e4
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-06 20:13:05 -0400

    consuming test service in controls controller

--- src/client/components/controls/controls.controller.js
+++ src/client/components/controls/controls.controller.js
@@ -2,10 +2,22 @@ class ControlsController {
   constructor(controlsService) {
   	this.controlsService = controlsService;
     this.name = 'Controller Component';
+    this.githubUsername = 'choshun';
+    this.result = {};
   }
 
   getServiceName() {
   	return this.controlsService.getName();
+  }
+
+  getDetails() {
+    this.controlsService.getItems(this.githubUsername).then((res) => {
+      this.result = res.data;
+      console.log('result?');
+      console.table(this.result.data.login);
+
+      this.setView(this.result.data.login);
+    });
   }
 
   setView(view) {
