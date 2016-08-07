9a0d273116deac0e76a8b6656d1865fcb43d5171
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-06 20:13:58 -0400

    adding example ajax consumption in service

--- src/client/components/controls/controls.service.js
+++ src/client/components/controls/controls.service.js
@@ -1,10 +1,28 @@
 class ControlsService {
-  constructor() {
+  constructor($http) {
     this.name = 'control service';
+    this.$http = $http;
   }
 
   getName() {
     return this.name;
+  }
+
+  getItems(githubUsername) {
+    const githubUrl = 'https://api.github.com';
+    return this.$http({
+      method: 'JSONP',
+      url: githubUrl + '/users/' +
+      githubUsername + '?callback=JSON_CALLBACK'
+    }).success(function(data) {
+      // this callback will be called asynchronously
+      // when the response is available
+      return data.data;
+    }).error(function(data, status) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      alert(status);
+    });
   }
 }
 
