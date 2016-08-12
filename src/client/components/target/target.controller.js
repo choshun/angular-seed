01fc25f3e53da0bc623377aaca49da0b717bdcd6
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-11 20:11:23 -0400

    adding on changes to controller

--- src/client/components/target/target.controller.js
+++ src/client/components/target/target.controller.js
@@ -2,10 +2,15 @@ class TargetController {
   constructor(targetService) {
   	this.targetService = targetService;
     this.name = 'Controller Component';
+    this.data = 'test';
   }
 
   getServiceName() {
   	return this.targetService.getName();
+  }
+
+  $onChanges(changesObj) {
+    console.log('fire target stuff!', changesObj);
   }
 }
 
