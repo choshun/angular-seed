f2b2e58727f6b6a60fdf531457a4a8ff267e8a67
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-08-12 10:58:00 -0400

    moving params to service (isolating controller)

--- src/client/components/graph/graph.controller.js
+++ src/client/components/graph/graph.controller.js
@@ -1,24 +1,17 @@
-import * as D3 from 'd3';
-
 class TargetController {
   constructor(graphService) {
-
     this.graphService = graphService;
     this.name = 'Controller Component';
     this.data = {};
     this.rawSvg = document.getElementById('graph');
-    this.d3 = D3;
-    this.svg = this.d3.select(this.rawSvg);
   }
 
   getServiceName() {
-  	return this.graphService.getName();
+    return this.graphService.getName();
   }
 
   $onChanges(changesObj) {
-    console.log('import?', this.data, changesObj);
-    // console.log('fire graph stuff!', typeof changesObj, this.svg);
-    this.graphService.graph(this.d3, this.svg, this.rawSvg, this.data);
+    this.graphService.graph(this.rawSvg, this.data);
   }
 }
 
--- src/client/components/graph/graph.service.js
+++ src/client/components/graph/graph.service.js
@@ -1,10 +1,12 @@
+import * as D3 from 'd3';
+
 class TargetService {
   constructor() {
     this.name = 'graph service';
 
-    this.d3 = {};
     this.padding = 20;
     this.pathClass = 'path';
+    this.svg = undefined;
     this.xScale = undefined;
     this.yScale = undefined;
     this.xAxisGen = undefined;
@@ -12,62 +14,61 @@ class TargetService {
     this.lineFun = undefined;
   }
 
-  // TODO: most of these should be private
-  graph(d3, svg, rawSvg, data) {
-    console.log('in service', data);
-    this.drawLineChart(d3, rawSvg, data.data, svg);
+  graph(rawSvg, data) {
+    this.svg = D3.select(rawSvg);
+    this.drawLineChart(rawSvg, data.data);
   }
 
   getName() {
     return this.name;
   }
 
-  setChartParameters(d3, rawSvg, data) {
-    this.xScale = d3.scaleLinear()
+  setChartParameters(rawSvg, data) {
+    this.xScale = D3.scaleLinear()
       .domain([data[0].hour, data[data.length - 1].hour])
       .range([this.padding + 5, rawSvg.clientWidth - this.padding]);
 
-    this.yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, function (d) {
+    this.yScale = D3.scaleLinear()
+      .domain([0, D3.max(data, function (d) {
         return d.sales;
       })])
     .range([rawSvg.clientHeight - this.padding, 0]);
 
-    this.xAxisGen = d3.axisBottom()
+    this.xAxisGen = D3.axisBottom()
       .scale(this.xScale)
       .ticks(data.length - 1);
 
-    this.yAxisGen = d3.axisLeft()
+    this.yAxisGen = D3.axisLeft()
       .scale(this.yScale)
       .ticks(5);
 
-    this.lineFun = d3.line()
+    this.lineFun = D3.line()
       .x((d) => this.xScale(d.hour))
       .y((d) => this.yScale(d.sales))
-      .curve(d3.curveCatmullRom.alpha(0.5));
+      .curve(D3.curveCatmullRom.alpha(0.5));
   }
 
-  drawLineChart(d3, rawSvg, data, svg) {
+  drawLineChart(rawSvg, data) {
     if (data === undefined) return;
 
-    this.setChartParameters(d3, rawSvg, data);
+    this.setChartParameters(rawSvg, data);
 
-    svg.append("svg:g")
-       .attr("class", "x axis")
-       .attr("transform", "translate(0,180)")
-       .call(this.xAxisGen);
+    this.svg.append('svg:g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,180)')
+      .call(this.xAxisGen);
 
-     svg.append("svg:g")
-        .attr("class", "y axis")
-        .attr("transform", "translate(20,0)")
-        .call(this.yAxisGen);
+    this.svg.append('svg:g')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(20,0)')
+      .call(this.yAxisGen);
 
-     svg.append("svg:path")
-        .attr("d", this.lineFun(data))
-        .attr("stroke", "blue")
-        .attr("stroke-width", 2)
-        .attr("fill", "none")
-        .attr("class", this.pathClass);
+    this.svg.append('svg:path')
+      .attr('d', this.lineFun(data))
+      .attr('stroke', 'blue')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none')
+      .attr('class', this.pathClass);
   }
 }
 
